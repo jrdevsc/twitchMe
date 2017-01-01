b39f8cbfef2b4db6506e586cf301fd632dc38229
julian rudolph <julian.rudolphsc@gmail.com>
2017-01-01 18:29:55 -0500

    styles, automacic fun stuff

--- scripts.js
+++ scripts.js
@@ -22,62 +22,41 @@ $('submit').on('click', function(e){
       console.log(sData.channels.length);
       for(var i = 0; i < sData.channels.length; i++){
         var sResultsName = sData.channels[i].display_name;
+        $('#sResults h2').removeClass('.hidden');
+        $('#sResultsP').append(`<a href="${bUrl+"users/"+sResultsName}/${client_id}">${sResultsName}</a>`+"<br>");
 
-        $('#sResults h4').append(`<a href="${bUrl+"users/"+sResultsName}/${client_id}">${sResultsName}</a>`+"<br>");
+        //reset form
+        $('inpu[type=text]').value = ' ';
       }
-
-      $('#sResults h2').removeClass('hidden');
     }
   });
 });
-  //build ajax call for search
-  // $.ajax({
-  //   url: bUrl + "search/channels",
-  //   data: {
-  //     query: query,
-  //     client_id: client_id
-  //   },
-  //   success: function(sData){
-  //     console.log(sData);
-  //   }
-  // });
-
-// });
-
 
 // this grabs the id of the user
 var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "potsuraze"];
 
-// users.forEach(function(user){
-//   $.ajax({
-//     url: bUrl+"users/"+user,
-//     data:{
-//       client_id: client_id,
-//     },
-//     success: function(data){
-//       //another ajax to see of the user is streaming
-//       $.ajax({
-//         url: bUrl+"channels/" +user,
-//         data:{
-//           client_id: client_id,
-//         },
-//         success: function(cData){
-//           if(cData.status == null){
-//             console.log(user + " is not streaming");
-//           }else{
-//             console.log(user + " is streaming. Status: "+cData.status);
-//             // console.log(cData);
-//           }
-//           // console.log(cData);
-//         }
-//       });
-//     }
-//   });
-// });
-
-
-
-
-// $('#fcc').click(function(){
-//
-// })
+users.forEach(function(user){
+  $.ajax({
+    url: bUrl+"users/"+user,
+    data:{
+      client_id: client_id,
+    },
+    success: function(data){
+      //another ajax to see of the user is streaming
+      $.ajax({
+        url: bUrl+"channels/" +user,
+        data:{
+          client_id: client_id,
+        },
+        success: function(cData){
+          if(cData.status == null){
+            console.log(user + " is not streaming");
+          }else{
+            $('#common').append("<div class='col-md-4'><div class='thumbnail'><img src="+cData.logo+"><div class='caption'><h3><a href="+cData.url+">"+cData.name+"</a></h3><p>Status: <em>"+cData.status+"</em></p></div></div></div>");
+            // console.log(cData);
+          }
+        }
+      });
+    }
+  });
+});
