111ce4dbfbe250ea410deb3fb07e8c536fb5a84a
julian rudolph <julian.rudolphsc@gmail.com>
2017-01-01 21:26:48 -0500

    looking to detect if user is streaming

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ $('submit').on('click', function(e){
       client_id: client_id,
     },
     success: function(sData){
-      console.log(sData.channels.length);
+      // console.log(sData.channels.length);
       for(var i = 0; i < sData.channels.length; i++){
         var sResultsName = sData.channels[i].display_name;
         $('#sResults h2').removeClass('.hidden');
@@ -33,7 +33,7 @@ $('submit').on('click', function(e){
 });
 
 // this grabs the id of the user
-var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "potsuraze"];
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "potsuraze", "Macie_Jay"];
 
 users.forEach(function(user){
   $.ajax({
@@ -42,6 +42,7 @@ users.forEach(function(user){
       client_id: client_id,
     },
     success: function(data){
+      // console.log(data);
       //another ajax to see of the user is streaming
       $.ajax({
         url: bUrl+"channels/" +user,
@@ -49,14 +50,29 @@ users.forEach(function(user){
           client_id: client_id,
         },
         success: function(cData){
+          // console.log(cData);
           if(cData.status == null){
-            console.log(user + " is not streaming");
+            // console.log(user + " is not streaming");
           }else{
-            $('#common').append("<div class='col-md-4'><div class='thumbnail'><img src="+cData.logo+"><div class='caption'><h3><a href="+cData.url+">"+cData.name+"</a></h3><p>Status: <em>"+cData.status+"</em></p></div></div></div>");
-            // console.log(cData);
-          }
+            $('#common').append("<div class='col-md-4'><div class='thumbnail'><img src="+cData.logo+"><div class='caption'><h3><a href="+cData.url+">"+cData.name+"</a></h3><p>Status: <em>"+cData.status+"</em></p><button class='btn btn-info'>Streaming?</button></div></div></div>");
+            $.ajax({
+                url: bUrl +"streams/",
+                data: {
+                  client_id: client_id,
+                },
+                success: function(sData){
+                  console.log(sData);
+                }
+            }); // end ajax
+          };
+        }
+        });
         }
       });
-    }
-  });
-});
+    });
+
+
+//button checks if user is streaming or not
+// $('button[class=qstreaming]').click(function(){
+//   console.log('click');
+// });
